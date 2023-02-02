34fb2bf53f4a99006e8984e5e4094fe640514322
namt93 <namt7115633@gmail.com>
2023-02-02 07:55:39 +0700

    Finish logic to add sidebar-item--active

--- frontend/weather-web-ui/src/components/Layout/DefaultLayout/Sidebar/index.js
+++ frontend/weather-web-ui/src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faCloudSun, faCog, fas, faThLarge } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faCloudSun, faCog, faTh } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 import styles from './Sidebar.module.scss';
@@ -15,36 +15,44 @@ function Sidebar() {
                     <i className={cx('sidebar__heading-icon')}>
                         <FontAwesomeIcon icon={faCloudSun} />
                     </i>
-                    <div className={cx('name-app')}>Wanru</div>
+                    <div className={cx('name-app')}> Wanru</div>
                 </h3>
                 <ul className={cx('sidebar-menu')}>
-                    <li className={cx('sidebar-item sidebar-item--active')}>
+                    <li className={cx('sidebar-item', { 'sidebar-item--active': window.location.pathname == '/' })}>
                         <Link to="/" className={cx('sidebar-item__link')}>
                             <div className={cx('sidebar-content')}>
                                 <i className={cx('sidebar-item-icon')}>
-                                    <FontAwesomeIcon icon={faThLarge} />
+                                    <FontAwesomeIcon icon={faTh} />
                                 </i>
-                                Dashboard
+                                <span className={cx('sidebar-item-title')}>Dashboard</span>
                             </div>
                         </Link>
                     </li>
-                    <li className={cx('sidebar-item')}>
+                    <li
+                        className={cx('sidebar-item', {
+                            'sidebar-item--active': window.location.pathname == '/calendar',
+                        })}
+                    >
                         <Link to="/calendar" className={cx('sidebar-item__link')}>
                             <div className={cx('sidebar-content')}>
                                 <i className={cx('sidebar-item-icon')}>
                                     <FontAwesomeIcon icon={faCalendar} />
                                 </i>
-                                Calendar
+                                <span className={cx('sidebar-item-title')}>Calendar</span>
                             </div>
                         </Link>
                     </li>
-                    <li className={cx('sidebar-item')}>
+                    <li
+                        className={cx('sidebar-item', {
+                            'sidebar-item--active': window.location.pathname == '/setting',
+                        })}
+                    >
                         <Link to="/setting" className={cx('sidebar-item__link')}>
                             <div className={cx('sidebar-content')}>
                                 <i className={cx('sidebar-item-icon')}>
                                     <FontAwesomeIcon icon={faCog} />
                                 </i>
-                                Setting
+                                <span className={cx('sidebar-item-title')}>Setting</span>
                             </div>
                         </Link>
                     </li>
